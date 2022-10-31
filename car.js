015afb20571997fc67460402c5752f985a0d807a
Marios Chartsias <96002652+MariosChartsias@users.noreply.github.com>
2022-10-31 18:26:18 +0200

    attempt 1 for sensors

--- car.js
+++ car.js
@@ -14,10 +14,12 @@ class Car{
         this.friction=0.05;
         this.angle=0;
 
+        this.sensor=new Sensor(this);
         this.controls= new Controls();
     }
     update(){
         this.#move();
+        this.sensor.update();
     }
     
     
@@ -84,5 +86,7 @@ class Car{
         ctx.fill();
         
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
